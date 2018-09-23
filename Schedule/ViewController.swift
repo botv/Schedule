86c8e78da3e501e7fec45c5e587dbba06badebd4
Ben Botvinick <benbotvinick@gmail.com>
2018-09-23 15:08:48 -0400

    Initial Commit

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Ben Botvinick on 9/23/18.
+//  Copyright © 2018 Ben Botvinick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
